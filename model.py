7554793d4c39afd4ef05455f74dec3abcaff24dd
alexisdrugeon <alexis.drugeon@gmail.com>
2023-03-09 15:31:56 +0100

    message

--- model.py
+++ model.py
@@ -6,7 +6,7 @@ import pickle
 
 # Simple dataset of three four features 
 # First of all load csv dataset file into dataframe
-dataset = pd.read_csv(?)
+dataset = pd.read_csv("C:\\Users\\Poney\\Desktop\\Deployement IA 2\\hiring.csv")
 
 #print the dataset
 
@@ -17,8 +17,9 @@ dataset['experience'].fillna(0, inplace=True)
 dataset['test_score'].fillna(dataset['test_score'].mean(), inplace=True)
 
 # now specify the Input features for training 
-X = dataset.iloc[]
-
+print(dataset)
+X = dataset.iloc[:,0:3]
+print(X)
 # Now some features are string/text, we need to convert them to number
 #Converting words to integer values
 def convert_to_int(word):
@@ -28,8 +29,8 @@ def convert_to_int(word):
 
 X['experience'] = X['experience'].apply(lambda x : convert_to_int(x))
 
-y = ?
-
+y = dataset.iloc[:,-1]
+print(y)
 #Splitting Training and Test Set
 #Since we have a very small dataset, we will train our model with all availabe data.
 
@@ -39,7 +40,7 @@ from sklearn.linear_model import LinearRegression
 regressor = LinearRegression()
 
 #Fitting model with trainig data
-regressor.?
+regressor.fit(X,y)
 
 # Saving model to disk
 pickle.dump(regressor, open('model.pkl','wb'))
